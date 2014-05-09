455bf4d7e3e744ae1200e7e3f290e3b63e7ef8a3
Wang Fengxue <cwfx91@gmail.com>
2014-05-09 16:14:53 +0800

    test

---
+++ test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: Wangfx91
+	> Mail:cwfx91@gmail.com 
+	> Created Time: Fri 09 May 2014 04:14:00 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("just a test");
+	return 0;
+}

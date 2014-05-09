da777ab925461d0b80ef152af0ff7d6ff448cf24
Wang Fengxue <cwfx91@gmail.com>
2014-05-09 15:49:15 +0800

    hello

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: Wangfx91
+	> Mail:cwfx91@gmail.com 
+	> Created Time: Fri 09 May 2014 03:48:16 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("helloworld\n");
+	return 0;
+}
